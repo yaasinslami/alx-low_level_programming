e3df110d308cfdfbf5807908df368176f36f368b
yaasinslami <yassinx4002@gmail.com>
2023-06-20 17:55:14 -0700

    int i, j;

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,20 +2,33 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
- *
  * @n: input
 */
 
 void print_to_98(int n)
 {
-	int count;
+int i, j;
 
-	if (n > 98)
-		for (count = n; count > 98; count--)
-			printf("%d, ", count);
-	else
-		for (count = n; count < 98; count++)
-			printf("%d, ", count);
-	printf("98\n");
+
+	if (n <= 98)
+		{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+		}
+		else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					printf("%d, ", j);
+				else if (j == 98)
+					printf("%d\n", j);
+			}
+		}
+
 }
