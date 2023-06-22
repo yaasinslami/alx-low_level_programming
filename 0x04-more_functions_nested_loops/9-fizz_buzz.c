934c572eeee140bc00eb98605daf2eab20904e34
yaasinslami <yassinx4002@gmail.com>
2023-06-22 16:04:47 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,7 +20,7 @@ int main(void)
 		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
 		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FrizzBuzz");
+			printf("FizzBuzz");
 		else
 			printf("%d", num);
 
