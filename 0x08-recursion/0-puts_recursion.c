640f1c5200ab5d6bbec887e57502fd4916685b26
yaasinslami <yassinx4002@gmail.com>
2023-07-05 05:42:57 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		putchar(*s);
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
-	else
-		putchar('\n');
 }
-
