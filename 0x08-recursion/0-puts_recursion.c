7d7cdd07a5278890234fb99ea88b2fa9f6439125
yaasinslami <yassinx4002@gmail.com>
2023-07-05 04:19:38 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		puts_recursion(s + 1);
+	}
+
+	else
+		putchar('\n');
+}
+
