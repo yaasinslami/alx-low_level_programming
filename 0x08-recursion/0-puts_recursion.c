f7abd0f09c19be3270d32d62a9952c9e2ddcf62b
yaasinslami <yassinx4002@gmail.com>
2023-07-05 04:21:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		putchar(*s);
-		puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
 	else
-		putchar('\n');
+		_putchar('\n');
 }
 
